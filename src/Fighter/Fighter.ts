b0ae695199e005253143bd52001ffda74ac4d420
Miguel <miguelanjohps@gmail.com>
2023-03-24 18:00:52 -0300

    fix: Corrige erro na interface Fighter Parâmetro "energy" retorna a ser opcional

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -3,8 +3,8 @@ import Energy from '../Energy';
 
 export default interface Fighter extends SimpleFighter {
   defense: number;
-  energy: Energy;
-
-  special?(enemy: Fighter): void;
+  energy?: Energy;
+  
+  special?(enemy: SimpleFighter): void;
   levelUp(): void;
 }
