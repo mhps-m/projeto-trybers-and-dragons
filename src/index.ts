3826c1a7bf91e48c226fbd69440d2d7ec3075d9e
Miguel <miguelanjohps@gmail.com>
2023-03-27 16:53:43 -0300

    feat: Conclui requisito 12 Cria arquivo index.ts com simulação de uma quest

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
+import Battle, { PVE, PVP } from './Battle';
 import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
 
-import { Orc } from './Races';
-import { Warrior } from './Archetypes';
-import getRandomInt from './utils';
+const player1 = new Character('Shulk');
+const player2 = new Character('Rex');
+const player3 = new Character('Noah');
 
-const a = new Character('Abu');
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
 
-console.log(a.race, a.archetype);
+const monster1 = new Monster();
+const monster2 = new Dragon();
 
-const b = new Character(
-  'Aba',
-  new Orc('Aba', getRandomInt(1, 10)),
-  new Warrior('Aba'),
-);
+const pvp = new PVP(player2, player3);
 
-console.log(b.race, b.archetype);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]) {
+  battles.forEach((battle) => battle.fight());
+}
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
